a70d2f6295083ac7cbef206855ae9e69ea3720a8
alex1iam <61625470+alex1iam@users.noreply.github.com>
2023-02-24 19:47:52 +1000

    Update tg_mqtt_bot.py

--- tg_mqtt_bot.py
+++ tg_mqtt_bot.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import telegram
 
 # Токен бота в телеграме
-bot_token = '1234567890:AAqwertyuioasdfghjklzxcvbn'
+bot_token = '1234567890:AAqwertyuiopasdfghjklzxcvbnm'
 # ID чата
 chat_id = '0987654321'
 # Создаем бота
@@ -20,7 +20,7 @@ mapp_topic_value = ""
 
 # Обработчик подключения к MQTT
 def on_connect(client, userdata, flags, rc):
-#    print("Connected with result code "+str(rc))
+    print("Connected with result code "+str(rc))
 
 # Функция для получения сообщения из топика
 def on_message(client, userdata, msg):
@@ -30,18 +30,20 @@ def on_message(client, userdata, msg):
     curr_topic_value = str(msg.payload.decode("utf-8"))
 #    print(msg.topic+" "+str(msg.payload.decode("utf-8")))
 #    print(curr_topic_value)
-    # Делаем мапинг (для человекочитаемости)
+    # Делаем мапинг содержимого топика (для человекочитаемости)
     if curr_topic_value == 'true':
      mapp_topic_value = 'Обнаружена протечка'
     if  curr_topic_value == 'false':
      mapp_topic_value = 'Протечка не обнаружена'
     # Проверяем, что значение содержимого топика изменилось
     if prev_topic_value != curr_topic_value:
-        # Отправляем сообщение в телеграмм
-        bot.send_message(chat_id=chat_id, text=msg.topic+":: "+mapp_topic_value)
-        # Обновляем предыдущее значение топика
-        prev_topic_value = curr_topic_value
-#        print(msg.topic+" "+str(msg.payload.decode("utf-8")))
+      # Отправляем сообщение в телеграмм
+      bot.send_message(chat_id=chat_id, text=msg.topic+":: "+mapp_topic_value)
+      # Также можно сделать подмену  названия топика на любую фразу:
+#     bot.send_message(chat_id=chat_id, text="Сюда впихнуть что хочется вместо названия топика mqtt: "+mapp_topic_value)
+      # Обновляем предыдущее значение топика
+      prev_topic_value = curr_topic_value
+#     print(msg.topic+" "+str(msg.payload.decode("utf-8")))
 
 # Устанавливаем функцию для обработки сообщений
 client.on_connect = on_connect
